de3f6875d31a7b40683a3c555dda83c6c5409dfc
UVTDX <thevagabond@tuta.io>
2024-08-31 03:30:52 +0300

    .

---
+++ main.asm
@@ -0,0 +1,21 @@
+format ELF64 executable 3
+
+segment readable executable
+
+entry main
+
+main:
+    lea  rdi, [msg]                               ; msg address to rdi
+    mov  rax, 14                                  
+    mov  rdx, rax                                 
+    mov  rsi, rdi                                 
+    mov  rdi, 1                                   ; stdout
+    mov  rax, 1                                   ; sys_write
+    syscall
+    xor  rdi, rdi                                 ; exit code 0
+    mov  rax, 60                                  ; sys_exit
+    syscall
+
+segment readable writable
+
+msg  db 'Hello world!', 10, 0
