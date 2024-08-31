c2434e778b335c7d2e33c59dd93f26e3c8b8c350
UVTDX <thevagabond@tuta.io>
2024-08-31 03:24:52 +0300

    .

--- main.hs
+++ main.hs
@@ -1,8 +1,7 @@
-module Main where
+
 
 -- A strange function to manipulate characters.
 strangeTransform :: Char -> Char
 strangeTransform c = toEnum (fromEnum c + 1)
 
--- A more abstract function to reverse the transformation.
-reverseStrangeTransform :: Char -> Char
+
