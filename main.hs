855c5f9bb3470f1478664b8b45170916ab329389
UVTDX <thevagabond@tuta.io>
2024-08-31 03:23:51 +0300

    .

--- main.hs
+++ main.hs
@@ -1,2 +1,17 @@
+module Main where
+
+-- A strange function to manipulate characters.
+strangeTransform :: Char -> Char
+strangeTransform c = toEnum (fromEnum c + 1)
+
+-- A more abstract function to reverse the transformation.
+reverseStrangeTransform :: Char -> Char
+reverseStrangeTransform c = toEnum (fromEnum c - 1)
+
+-- Another layer of abstraction and obfuscation.
+weirdString :: String -> String
+weirdString = map strangeTransform
+
+-- Main function with more obfuscation.
 main :: IO ()
-main = putStrLn . map reverseStrangeTransform . weirdString $ "Ifmmp-!Xpsme""
+main = putStrLn . map reverseStrangeTransform . weirdString $ "Hello, World!"
